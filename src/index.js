1f0a86e22674b7d46cff88a5eecab68966522139
ArvinthS18 <your_email_address@example.com>
2022-12-07 17:57:26 +0530

    first commit2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ringer from "./Anime WOW sound.mp3";
+import ringer from "./Wow Wow Wubzy.mp3";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const Sound = () => {
   const audio = new Audio(ringer);
@@ -9,15 +11,37 @@ const Sound = () => {
 
   return (
     <div>
-      <button
-        onClick={() => {
-          audio.loop = true;
-          audio.play();
-        }}
-      >
-        Play
-      </button>
-      <button onClick={() => (audio.loop = false)}>Pause</button>
+      <br></br>
+      <h2 className="normal" align="center" style={{ color: "red" }}>
+        <b>Sound Player</b>
+      </h2>
+      <h3 align="center" style={{ color: "green" }}>
+        <b>click the button to hear!</b>
+      </h3>
+      <center>
+        {" "}
+        <button
+          type="button"
+          class="btn btn-primary"
+          onClick={() => {
+            audio.loop = true;
+            audio.play();
+          }}
+        >
+          Click to Play
+        </button>
+      </center>
+
+      <br></br>
+      <center>
+        <button
+          type="button"
+          class="btn btn-primary"
+          onClick={() => (audio.loop = false)}
+        >
+          Click to Pause
+        </button>
+      </center>
     </div>
   );
 };
